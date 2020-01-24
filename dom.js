76b713f8941563673b2670b4ec32690ffff222b4
wildintu <hello@wildintu.com>
2020-01-24 16:17:20 -0600

    adding sibling elements

--- dom.js
+++ dom.js
@@ -1,3 +1,14 @@
-$('.button-class').click(function(){
-    console.log('a button with class "button-class" was clicked.')
+//both are sibling elements
+function addListItem() {
+    let list = document.getElementById('grocery-list');
+    let item = document.createElement('li');
+    item.innerText = 'Milk and bread';
+    list.appendChild(item);
+}
+
+$('#button-id').click(function() {
+    let item = '<li>Apples and oranges</li>';
+    $('#grocery-list').append(item);
+    //or option below
+    //$('#grocery-list').append('<li>Apples and oranges</li>');
 });
