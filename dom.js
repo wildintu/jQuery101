7ffeb2c747caaa5bc4fba51f82a30c6e4987a974
wildintu <hello@wildintu.com>
2020-01-24 16:26:33 -0600

    more changes

--- dom.js
+++ dom.js
@@ -7,8 +7,20 @@ function addListItem() {
 }
 
 $('#button-id').click(function() {
-    let item = '<li>Apples and oranges</li>';
+    let item = '<li class="red">Apples and oranges</li>';
     $('#grocery-list').append(item);
     //or option below
     //$('#grocery-list').append('<li>Apples and oranges</li>');
+});
+
+$('#style-me').click(function() {
+    $('.red').css('color', 'red');
+});
+
+$(document).ready(function() {
+    $('<div></div>').css({
+        'background-color': 'red',
+        'height': '100px',
+        'width': '100px'
+    }).insertAfter('#grocery-list');
 });
